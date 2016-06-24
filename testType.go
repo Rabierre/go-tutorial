aaeb07ebb69e27f405b1cf77791ad34b22678597
Jihye Seo <rabierre@gmail.com>
2016-06-25 05:32:09 +0900

    Initial commit with some examples

---
+++ testType.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+  testType := "hello world"
+  print(testType)
+
+  testType = 1
+  print(testType)
+}
---
+++ testUnusedVar.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+  someVar := "Hello I do nothing"
+}
